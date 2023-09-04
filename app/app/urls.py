96aa71b1c548e9c7857117b8587422addf5ffd13
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-09-04 18:16:56 +0530

    error solved using code checker

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-schema'
+        name='api-docs',
         ),
 ]
 
