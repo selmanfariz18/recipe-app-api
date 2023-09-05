810c4c758d8c9bf06950121b9a156b8d9bb16472
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-09-05 19:43:33 +0530

    recipe model created

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 # database models
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -38,6 +39,22 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
 
-    objects = UserManager()
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
